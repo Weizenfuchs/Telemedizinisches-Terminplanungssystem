ead5bce2341643d600ba964ef2c4f30fef44b7f9
Joachim Fuchs <jf@v2a.net>
2025-05-17 13:08:10 +0200

    phinx config

---
+++ phinx.php
@@ -0,0 +1,22 @@
+<?php
+
+return
+[
+    'paths' => [
+        'migrations' => 'db/migrations',
+        'seeds' => 'db/seeds'
+    ],
+    'environments' => [
+        'default_environment' => 'development',
+        'development' => [
+            'adapter' => 'pgsql',
+            'host' => 'telemedizin-db',
+            'name' => 'telemedizin',
+            'user' => 'fuchs',
+            'pass' => 'schwarzwald',
+            'port' => '5432',
+            'charset' => 'utf8',
+        ],
+    ],
+    'version_order' => 'creation'
+];
